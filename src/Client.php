92774bc19ed3e975e05ed0eb4ba0e119fe6b4d9e
erika <efockereinaldo@digitalocean.com>
2020-02-24 11:15:49 +0100

    Fixed namespace and other details

--- src/Client.php
+++ src/Client.php
@@ -1,18 +1,20 @@
 <?php
 
-namespace Minicli\Util;
+namespace Minicli\Curly;
 
 class Client
 {
-    protected $last_response;
+    /** @var array */
+    protected $last_request_info;
 
     /**
      * Makes a GET query
      * @param string $endpoint API endpoint
      * @param array $headers optional headers
-     * @return mixed
+     * @param bool $header_out
+     * @return array
      */
-    public function get($endpoint, array $headers = [])
+    public function get($endpoint, array $headers = [], $header_out = false)
     {
         $curl = curl_init();
 
@@ -20,7 +22,7 @@ class Client
             CURLOPT_HTTPHEADER => $headers,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_URL => $endpoint,
-            CURLINFO_HEADER_OUT => true
+            CURLINFO_HEADER_OUT => $header_out
         ]);
 
         return $this->getQueryResponse($curl);
@@ -31,9 +33,10 @@ class Client
      * @param $endpoint
      * @param array $params
      * @param array $headers
+     * @param bool $header_out
      * @return array
      */
-    public function post($endpoint, array $params, $headers = [])
+    public function post($endpoint, array $params, $headers = [], $header_out = false)
     {
         $curl = curl_init();
 
@@ -43,7 +46,7 @@ class Client
             CURLOPT_POST => true,
             CURLOPT_POSTFIELDS => json_encode($params),
             CURLOPT_URL => $endpoint,
-            #CURLINFO_HEADER_OUT => true,
+            CURLINFO_HEADER_OUT => $header_out,
             CURLOPT_TIMEOUT => 120,
         ]);
 
@@ -54,9 +57,10 @@ class Client
      * Makes a DELETE query
      * @param $endpoint
      * @param array $headers
+     * @param bool $header_out
      * @return array
      */
-    public function delete($endpoint, $headers = [])
+    public function delete($endpoint, $headers = [], $header_out = false)
     {
         $curl = curl_init();
 
@@ -65,21 +69,23 @@ class Client
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_CUSTOMREQUEST => "DELETE",
             CURLOPT_URL => $endpoint,
-            #CURLINFO_HEADER_OUT => true,
+            CURLINFO_HEADER_OUT => $header_out,
         ]);
 
         return $this->getQueryResponse($curl);
     }
 
     /**
-     * Exec curl and get response
+     * Execs curl and saves information about the request.
+     * Returns an array with two items: 'code' and 'body'.
      * @param $curl
      * @return array
      */
     protected function getQueryResponse($curl)
     {
         $response = curl_exec($curl);
-        $response_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $this->last_request_info = curl_getinfo($curl);
+        $response_code = $this->last_request_info['http_code'];
 
         curl_close($curl);
 
